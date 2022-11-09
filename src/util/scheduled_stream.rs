26c6eba4105637b76c43b093244cea718b350585
Torsten Schlieder <torsten.schlieder@unite.eu>
2022-11-09 09:08:55 +0100

    Started to add tests to scheduled_stream (not working)

--- src/util/scheduled_stream.rs
+++ src/util/scheduled_stream.rs
@@ -75,11 +75,52 @@ impl Stream for ScheduledStream {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+    use futures_util::StreamExt;
+    use crate::util::scheduled_stream::{Generator, ScheduledStream};
+
+    struct TestGenerator {
+        batches: Vec<Vec<&'static str>>,
+        index: usize
+    }
+
+    impl TestGenerator {
+        fn new(batches: Vec<Vec<&'static str>>) -> Self {
+            Self { batches, index: 0 }
+        }
+    }
+
+    impl Generator for TestGenerator {
+        fn generate(&mut self) -> Vec<String> {
+            if self.index == self.batches.len() {
+                return Vec::new()
+            }
+            let iter = self.batches[self.index].iter();
+            self.index += 1;
+            iter.map(|y| String::from(*y)).collect()
+        }
+    }
+
+    #[tokio::test]
+    async fn test_empty_first_batch() {
+        let d = vec![vec!["1"]];
+        let g = Box::new(TestGenerator::new(d));
+        let mut s = ScheduledStream::new(Duration::from_millis(3), g);
+        while let Some(item) = s.next().await {
+            println!("-----> {}", item)
+        }
+    }
+}
+
+/*
 #[tokio::main]
 async fn main() {
     let g = Box::new(RandomGenerator::new(5));
     let mut s = ScheduledStream::new(Duration::from_secs(3), g);
     while let Some(item) = s.next().await {
-        println!("---> {}", item)
+        println!("-----> {}", item)
     }
 }
+*/
