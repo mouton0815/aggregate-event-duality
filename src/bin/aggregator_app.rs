d9efc3babd2c05f27d29a6c4a5da2bb81621ee55
Torsten Schlieder <torsten.schlieder@unite.eu>
2022-11-16 12:15:28 +0100

    Simplified spawning of HTTP server (no rx/tx needed because no other thread is active)

--- src/bin/aggregator_app.rs
+++ src/bin/aggregator_app.rs
@@ -1,41 +1,15 @@
-use std::error::Error;
+extern crate core;
+
 use std::sync::{Arc, Mutex};
-use tokio::signal;
-use tokio::sync::broadcast;
 use aggregate_event_duality::aggregator::company_aggregator::CompanyAggregator;
 use aggregate_event_duality::rest::http_server::spawn_http_server;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() {
 
-    let aggregator = CompanyAggregator::new(":memory:")?;
+    let aggregator = CompanyAggregator::new(":memory:")
+        .unwrap_or_else(|error| panic!("{}", error));
     let aggregator= Arc::new(Mutex::new(aggregator));
 
-    let (tx, rx) = broadcast::channel(1);
-
-    let handle = spawn_http_server(aggregator, rx);
-    tokio::pin!(handle);
-
-    loop {
-        tokio::select! {
-            _ = &mut handle => {
-                println!("HTTP Server terminated");
-                break;
-            }
-            s = signal::ctrl_c() => {
-                match s {
-                    Ok(()) => {
-                        println!("Termination signal received");
-                        tx.send(())?;
-                    },
-                    Err(err) => {
-                        eprintln!("Unable to listen for shutdown signal: {}", err);
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    Ok(())
+    spawn_http_server(aggregator).await;
 }
--- src/rest/http_server.rs
+++ src/rest/http_server.rs
@@ -1,6 +1,4 @@
 use std::convert::Infallible;
-use tokio::sync::broadcast::Receiver;
-use tokio::task::JoinHandle;
 use warp::Filter;
 use crate::rest::rest_handlers::{get_companies, MutexedCompanyAggregator, post_company, patch_company, get_company_events};
 
@@ -9,7 +7,7 @@ fn with_aggregator(aggregator: MutexedCompanyAggregator)
     warp::any().map(move || aggregator.clone())
 }
 
-pub fn spawn_http_server(aggregator: MutexedCompanyAggregator, mut rx: Receiver<()>) -> JoinHandle<()> {
+pub async fn spawn_http_server(aggregator: MutexedCompanyAggregator) {
     println!("Spawn HTTP server");
 
     let path = "companies";
@@ -42,11 +40,7 @@ pub fn spawn_http_server(aggregator: MutexedCompanyAggregator, mut rx: Receiver<
         .or(route_patch_company)
         .or(route_get_company_events);
 
-    let (_, server) = warp::serve(routes)
-        .bind_with_graceful_shutdown(([127, 0, 0, 1], 3000), async move {
-            rx.recv().await.unwrap();
-            println!("Termination signal received, leave HTTP server");
-        });
-
-    tokio::spawn(server)
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3000))
+        .await;
 }
