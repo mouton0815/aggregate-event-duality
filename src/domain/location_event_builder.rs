274653ef242dd84b77d73c42284864e23c39076d
Torsten Schlieder <torsten.schlieder@unite.eu>
2022-12-03 14:15:40 +0100

    Towards a standard interface for aggregate builders

--- src/domain/location_event_builder.rs
+++ src/domain/location_event_builder.rs
@@ -14,31 +14,33 @@ impl LocationEventBuilder {
         }
     }
 
-    pub fn for_update(person_id: u32, person_patch: &PersonPatch, person_data: &PersonData, old_location: Option<&str>, is_last_in_aggregate: bool) -> Option<String> {
-        let new_location = person_patch.location.as_ref();
+    pub fn for_update(person_id: u32, before: &PersonData, patch: &PersonPatch, after: &PersonData, is_last_in_aggregate: bool) -> Option<String> {
+        let old_location = before.location.as_ref();
+        let new_location = patch.location.as_ref();
         if old_location.is_none() && !new_location.is_value() {
             // No location information before and after
             None
         } else if old_location.is_none() && new_location.is_value() {
             // Update sets a location
-            Self::stringify(LocationEvent::for_insert_person(new_location.unwrap(), person_id, person_data))
+            Self::stringify(LocationEvent::for_insert_person(new_location.unwrap(), person_id, after))
         } else if new_location.is_null() {
             // Update clears the location
             Self::stringify(LocationEvent::for_delete_person(old_location.unwrap(), person_id, is_last_in_aggregate))
         } else if new_location.is_absent() || new_location.is_value() && old_location.unwrap() == new_location.unwrap() {
             // Update keeps the location
-            Self::stringify(LocationEvent::for_update_person(old_location.unwrap(), person_id, person_patch))
+            Self::stringify(LocationEvent::for_update_person(old_location.unwrap(), person_id, patch))
         } else {
             // Update changes the location
-            Self::stringify(LocationEvent::for_move_person(old_location.unwrap(), new_location.unwrap(), person_id, person_data, is_last_in_aggregate))
+            Self::stringify(LocationEvent::for_move_person(old_location.unwrap(), new_location.unwrap(), person_id, after, is_last_in_aggregate))
         }
     }
 
-    pub fn for_delete(person_id: u32, old_location: Option<&str>, is_last_in_aggregate: bool) -> Option<String> {
-        if old_location.is_none() {
+    pub fn for_delete(person_id: u32, person: &PersonData, is_last_in_aggregate: bool) -> Option<String> {
+        let location = person.location.as_ref();
+        if location.is_none() {
             None
         } else {
-            Self::stringify(LocationEvent::for_delete_person(old_location.unwrap(), person_id, is_last_in_aggregate))
+            Self::stringify(LocationEvent::for_delete_person(location.unwrap(), person_id, is_last_in_aggregate))
         }
     }
 
@@ -56,7 +58,7 @@ mod tests {
 
     #[test]
     pub fn test_insert_event_no_location() {
-        let person = PersonData::new("Hans", None, None);
+        let person = PersonData::new("", None, None);
         let result = LocationEventBuilder::for_insert(5, &person);
         assert_eq!(result, None); // No event created
     }
@@ -70,77 +72,82 @@ mod tests {
 
     #[test]
     pub fn test_update_event_no_location() {
+        let person = PersonData::new("", None, None);
         let patch = PersonPatch::new(None, Patch::Null, Patch::Null);
-        let after = PersonData::new("Hans", None, None);
-        let result = LocationEventBuilder::for_update(5, &patch, &after, None, false);
+        let result = LocationEventBuilder::for_update(5, &person, &patch, &person, false);
         assert_eq!(result, None); // No event created
     }
 
     #[test]
     pub fn test_update_event_set_location() {
+        let before = PersonData::new("", None, None);
         let patch = PersonPatch::new(None, Patch::Value("foo"), Patch::Absent);
         let after = PersonData::new("Hans", Some("foo"), Some(123));
-        let result = LocationEventBuilder::for_update(5, &patch, &after, None, false);
+        let result = LocationEventBuilder::for_update(5, &before, &patch, &after, false);
         assert_eq!(result, Some(r#"{"foo":{"5":{"name":"Hans","location":"foo","spouseId":123}}}"#.to_string()));
     }
 
     #[test]
     pub fn test_update_event_keep_location() {
-        let patch = PersonPatch::new(Some("Hans"), Patch::Absent, Patch::Absent);
-        let after = PersonData::new("Hans", Some("foo"), Some(123));
-        let result = LocationEventBuilder::for_update(5, &patch, &after, Some("foo"), false);
-        assert_eq!(result, Some(r#"{"foo":{"5":{"name":"Hans"}}}"#.to_string()));
+        let person = PersonData::new("", Some("foo"), None);
+        let patch = PersonPatch::new(Some("Hans"), Patch::Absent, Patch::Value(123));
+        let result = LocationEventBuilder::for_update(5, &person, &patch, &person, false);
+        assert_eq!(result, Some(r#"{"foo":{"5":{"name":"Hans","spouseId":123}}}"#.to_string()));
     }
 
     #[test]
     pub fn test_update_event_same_location() {
+        let person = PersonData::new("", Some("foo"), None);
         let patch = PersonPatch::new(None, Patch::Value("foo"), Patch::Absent);
-        let after = PersonData::new("Hans", Some("foo"), Some(123));
-        let result = LocationEventBuilder::for_update(5, &patch, &after, Some("foo"), false);
+        let result = LocationEventBuilder::for_update(5, &person, &patch, &person, false);
         assert_eq!(result, Some(r#"{"foo":{"5":{"location":"foo"}}}"#.to_string()));
     }
 
     #[test]
     pub fn test_update_event_change_location() {
+        let before = PersonData::new("", Some("foo"), None);
         let patch = PersonPatch::new(None, Patch::Value("bar"), Patch::Absent);
         let after = PersonData::new("Hans", Some("bar"), None);
-        let result = LocationEventBuilder::for_update(5, &patch, &after, Some("foo"), false);
+        let result = LocationEventBuilder::for_update(5, &before, &patch, &after, false);
         assert_eq!(result, Some(r#"{"bar":{"5":{"name":"Hans","location":"bar"}},"foo":{"5":null}}"#.to_string()));
     }
 
     #[test]
     pub fn test_update_event_change_last_location() {
+        let before = PersonData::new("", Some("foo"), None);
         let patch = PersonPatch::new(None, Patch::Value("bar"), Patch::Absent);
         let after = PersonData::new("Hans", Some("bar"), None);
-        let result = LocationEventBuilder::for_update(5, &patch, &after, Some("foo"), true);
+        let result = LocationEventBuilder::for_update(5, &before, &patch, &after, true);
         assert_eq!(result, Some(r#"{"bar":{"5":{"name":"Hans","location":"bar"}},"foo":null}"#.to_string()));
     }
 
     #[test]
     pub fn test_update_event_remove_location() {
+        let person = PersonData::new("", Some("foo"), None);
         let patch = PersonPatch::new(None, Patch::Null, Patch::Absent);
-        let after = PersonData::new("Hans", None, Some(123));
-        let result = LocationEventBuilder::for_update(5, &patch, &after, Some("foo"), false);
+        let result = LocationEventBuilder::for_update(5, &person, &patch, &person, false);
         assert_eq!(result, Some(r#"{"foo":{"5":null}}"#.to_string()));
     }
 
     #[test]
     pub fn test_update_event_remove_last_location() {
+        let person = PersonData::new("", Some("foo"), None);
         let patch = PersonPatch::new(None, Patch::Null, Patch::Absent);
-        let after = PersonData::new("Hans", None, Some(123));
-        let result = LocationEventBuilder::for_update(5, &patch, &after, Some("foo"), true);
+        let result = LocationEventBuilder::for_update(5, &person, &patch, &person, true);
         assert_eq!(result, Some(r#"{"foo":null}"#.to_string()));
     }
 
     #[test]
     pub fn test_delete_event_remove_location() {
-        let result = LocationEventBuilder::for_delete(5, Some("foo"), false);
+        let person = PersonData::new("", Some("foo"), None);
+        let result = LocationEventBuilder::for_delete(5, &person, false);
         assert_eq!(result, Some(r#"{"foo":{"5":null}}"#.to_string()));
     }
 
     #[test]
     pub fn test_delete_event_remove_last_location() {
-        let result = LocationEventBuilder::for_delete(5, Some("foo"), true);
+        let person = PersonData::new("", Some("foo"), None);
+        let result = LocationEventBuilder::for_delete(5, &person, true);
         assert_eq!(result, Some(r#"{"foo":null}"#.to_string()));
     }
 }
