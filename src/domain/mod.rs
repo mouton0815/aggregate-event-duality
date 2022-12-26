ba585d68e68c6201c0834d8e1a2989a6dae07b39
Torsten Schlieder <torsten.schlieder@unite.eu>
2022-12-26 20:56:33 +0100

    Location data and patch with ctors and tests

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,7 +1,9 @@
-pub mod person_event;
-pub mod location_map;
-pub mod person_map;
 pub mod person_data;
 pub mod person_patch;
+pub mod person_event;
+pub mod person_map;
+pub mod location_data;
+pub mod location_patch;
 pub mod location_event;
 pub mod location_event_builder;
+pub mod location_map;
