6d7880eb58b5703d140034b345518d1436216d29
Torsten Schlieder <torsten.schlieder@unite.eu>
2022-12-31 16:52:26 +0100

    Greatly simplified termination logic in server.rs

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,8 @@
-use log::{debug, info, warn};
+use log::{debug, info};
 use std::error::Error;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
-use tokio::signal;
+use tokio::{join, signal};
 use tokio::sync::broadcast;
 use aggregate_event_duality::aggregator::aggregator_facade::AggregatorFacade;
 use aggregate_event_duality::rest::http_server::spawn_http_server;
@@ -15,41 +15,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let aggregator = AggregatorFacade::new(":memory:")?;
     let aggregator= Arc::new(Mutex::new(aggregator));
 
-    let (tx, rx_scheduler) = broadcast::channel(1);
-    let rx_http_server = tx.subscribe();
+    let (tx, rx1) = broadcast::channel(1);
+    let rx2 = tx.subscribe();
 
     let period = Duration::from_secs(10);
     let deletion_task: MutexDeletionTask<rusqlite::Error> = aggregator.clone(); // Aggregator implements trait DeletionTask
-    let scheduler_handle = spawn_deletion_scheduler(&deletion_task, rx_scheduler, period);
-    tokio::pin!(scheduler_handle);
+    let delete_scheduler = spawn_deletion_scheduler(&deletion_task, rx1, period);
 
-    let http_server_handle = spawn_http_server(&aggregator, rx_http_server, 5);
-    tokio::pin!(http_server_handle);
+    let http_server = spawn_http_server(&aggregator, rx2, 5);
 
-    loop {
-        tokio::select! {
-            _ = &mut scheduler_handle => {
-                info!("Deletion scheduler terminated");
-                break;
-            }
-            _ = &mut http_server_handle => {
-                info!("HTTP Server terminated");
-                break;
-            }
-            s = signal::ctrl_c() => {
-                match s {
-                    Ok(()) => {
-                        debug!("Termination signal received");
-                        tx.send(())?;
-                    },
-                    Err(err) => {
-                        warn!("Unable to listen for shutdown signal: {}", err);
-                        break;
-                    }
-                }
-            }
-        }
-    }
+    signal::ctrl_c().await?;
+    debug!("Termination signal received");
+    tx.send(())?;
+
+    let (_,_) = join!(delete_scheduler, http_server);
+    info!("Deletion scheduler terminated");
+    info!("HTTP Server terminated");
 
     Ok(())
 }
