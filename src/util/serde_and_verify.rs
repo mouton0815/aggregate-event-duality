65016ec7ff4bfea80fd89e437bbd16c21427fa39
Torsten Schlieder <torsten.schlieder@unite.eu>
2023-01-01 13:19:40 +0100

    Documented serde_and_verify

--- src/util/serde_and_verify.rs
+++ src/util/serde_and_verify.rs
@@ -3,17 +3,22 @@ pub mod tests {
     use std::fmt::Debug;
     use serde::{Deserialize, Serialize};
 
-    pub fn serde_and_verify<'de, Data>(data_ref: &Data, json_ref: &'de str)
+    ///
+    /// Helper function for serialization/deserialization tests.
+    /// First serializes ``data`` and compares the result with ``json``.
+    /// Then deserializes ``json`` into an object of type ``Data`` and compares the result with ``data``.
+    ///
+    pub fn serde_and_verify<'de, Data>(data: &Data, json: &'de str)
         where Data: Debug + Deserialize<'de> + Serialize + PartialEq {
 
-        // 1. Serialize data_ref and string-compare it to json_ref
-        let json = serde_json::to_string(&data_ref);
-        assert!(json.is_ok());
-        assert_eq!(json.unwrap(), String::from(json_ref));
+        // 1. Serialize data and string-compare it to json
+        let json_result = serde_json::to_string(&data);
+        assert!(json_result.is_ok());
+        assert_eq!(json_result.unwrap(), String::from(json));
 
-        // 2. Deserialize the serialized json and compare it with data_ref
-        let data: Result<Data, serde_json::Error> = serde_json::from_str(json_ref);
-        assert!(data.is_ok());
-        assert_eq!(data.unwrap(), *data_ref);
+        // 2. Deserialize json and compare it with data
+        let data_result: Result<Data, serde_json::Error> = serde_json::from_str(json);
+        assert!(data_result.is_ok());
+        assert_eq!(data_result.unwrap(), *data);
     }
 }
