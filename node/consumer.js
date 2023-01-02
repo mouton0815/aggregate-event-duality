bb097efe8629c6700abf90e55e1197ee34ec3754
Torsten Schlieder <torsten.schlieder@unite.eu>
2023-01-02 11:53:55 +0100

    Documentation of node.js consumer

--- node/consumer.js
+++ node/consumer.js
@@ -7,6 +7,10 @@ import Merger from 'json-merge-patch'
 const HOST = 'http://localhost:3000'
 const REVISION_HEADER = 'X-Revision'
 
+// Bare minimum implementation of a consumer that builds and maintains a read model.
+// The read model ("persons") is initialized with person aggregates fetched from the REST API.
+// It is subsequently kept up-to-date with person events consumed from the corresponding SSE channel.
+// The events are merged into the read model with help of library 'json-merge-patch'.
 try {
     const response = await fetch(`${HOST}/persons`)
     const revision = Number(response.headers.get(REVISION_HEADER)) || 0
